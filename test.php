1ec0420d257a0a727782c0f67fb5f536cf6cd8bf
tuantt <sangtd@kaopiz.com>
2017-03-15 14:24:26 +0700

    change to same server mysql version

--- test.php
+++ test.php
@@ -6,19 +6,18 @@ define("PASSWORD", "spicesmart");
 define("DATABASENAME", "newspicemart");
 
 
-backup_tables(HOST,USERNAME,PASSWORD,DATABASENAME, 'user');
+backup_tables(HOST,USERNAME,PASSWORD,DATABASENAME,'user');
 
 /* backup the db OR just a table */
 function backup_tables($host,$user,$pass,$name,$tables = '*')
 {
-	$link = mysqli_connect($host,$user,$pass);
-	mysqli_select_db($link,$name);
-	
+	$link = mysql_connect($host,$user,$pass);
+	mysql_select_db($name,$link);
 	//get all of the tables
 	if($tables == '*')
 	{
 		$tables = array();
-		$result = mysqli_query('SHOW TABLES');
+		$result = mysql_query('SHOW TABLES');
 		while($row = mysql_fetch_row($result))
 		{
 			$tables[] = $row[0];
@@ -29,14 +28,15 @@ function backup_tables($host,$user,$pass,$name,$tables = '*')
 		$tables = is_array($tables) ? $tables : explode(',',$tables);
 	}
 	$return = '';
+
 	//cycle through
 	foreach($tables as $table)
 	{
-		$result = mysqli_query('SELECT * FROM '.$table);
+		$result = mysql_query('SELECT * FROM '.$table);
 		$num_fields = mysql_num_fields($result);
 		// var_dump($tables);die;
 		$return.= 'DROP TABLE '.$table.';';
-		$row2 = mysql_fetch_row(mysqli_query('SHOW CREATE TABLE '.$table));
+		$row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
 		$return.= "\n\n".$row2[1].";\n\n";
 		
 		for ($i = 0; $i < $num_fields; $i++) 
