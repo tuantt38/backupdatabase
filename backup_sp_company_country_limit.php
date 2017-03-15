0b9d95191a78260e136492a0741e83aef1db2dc3
tuantt <sangtd@kaopiz.com>
2017-03-15 13:13:58 +0700

    change to same server mysql version

--- backup_sp_company_country_limit.php
+++ backup_sp_company_country_limit.php
@@ -11,14 +11,14 @@ backup_tables(HOST,USERNAME,PASSWORD,DATABASENAME,'sp_company_country_limit');
 /* backup the db OR just a table */
 function backup_tables($host,$user,$pass,$name,$tables = '*')
 {
-	$link = mysql_connect($host,$user,$pass);
-	mysql_select_db($name,$link);
+	$link = mysqli_connect($host,$user,$pass);
+	mysqli_select_db($name,$link);
 	
 	//get all of the tables
 	if($tables == '*')
 	{
 		$tables = array();
-		$result = mysql_query('SHOW TABLES');
+		$result = mysqli_query('SHOW TABLES');
 		while($row = mysql_fetch_row($result))
 		{
 			$tables[] = $row[0];
@@ -32,11 +32,11 @@ function backup_tables($host,$user,$pass,$name,$tables = '*')
 	//cycle through
 	foreach($tables as $table)
 	{
-		$result = mysql_query('SELECT * FROM '.$table);
+		$result = mysqli_query('SELECT * FROM '.$table);
 		$num_fields = mysql_num_fields($result);
 		// var_dump($tables);die;
 		$return.= 'DROP TABLE '.$table.';';
-		$row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
+		$row2 = mysql_fetch_row(mysqli_query('SHOW CREATE TABLE '.$table));
 		$return.= "\n\n".$row2[1].";\n\n";
 		
 		for ($i = 0; $i < $num_fields; $i++) 
